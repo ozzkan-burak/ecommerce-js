b2a750326b82c7c6b6c81349b2e94d2e4b0dee25
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-05-09 23:37:05 +0300

    episode10

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,4 @@
-export default {
+const data = {
   products: [
     {
       _id: '1',
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,15 +1,15 @@
-// const path = require('path');
+const path = require('path');
     
-// module.exports = {
-//     entry: ' ./src/index.js',
-//     output: {
-//         filename: 'main.js',
-//         path: path.resolve(__dirname, 'dist')
-//     },
-//     devServer: {
-//         static: path.resolve(__dirname, 'dist'),
-//         compress: true,
-//         port: 8080,
-//     },
-//     mode: 'development'
-// };
+module.exports = {
+    entry: ' ./src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 8080,
+    },
+    mode: 'development'
+};
