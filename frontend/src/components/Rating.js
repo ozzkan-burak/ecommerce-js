26efcfa9a5fa0ca6bef3ddcec6d30de733182a6e
burak.ozkan <burak.ozkan@flo.com.tr>
2022-11-01 00:12:10 +0300

    episode38

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -4,7 +4,7 @@ const Rating = {
       return `<div></div>`;
     }
     return `
-      <div class="reting">
+      <div class="rating">
         <span>
           <i class="${props.value >= 1 ? 'fa fa-star' : props.value >= 0.5 ? 'fa fa-star-half-o': 'fa fa-star-half'}"></i>
         </span>
@@ -27,3 +27,5 @@ const Rating = {
     `;
   },
 };
+
+export default Rating;
